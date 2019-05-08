9f7a2a080f438c86be40a653ed79f498b39ef018
Tzvi Seliger <tzviseliger@gmail.com>
2019-05-07 22:45:59 -0400

    saving

---
+++ EmployeeAppBack/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeAppBack.Models
+{
+    public class Accounts
+    {
+        public int AccountID { get; set; }
+        public string AccountName { get; set; }
+        public string AccountDescription { get; set; }
+        public string AccountLogo { get; set; }
+    }
+}
